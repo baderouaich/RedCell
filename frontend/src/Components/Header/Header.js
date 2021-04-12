1ae92e6c3b0a25b43a57f62d4f44ca5e168efb0e
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-04-12 17:02:49 +0000

    Header styling
    added shadow and logo

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Header.scss'
-
+import Logo from '../../Resources/Images/Logo.png'
 const Header = props => 
 {
     return (
         <header className="header">
-            I am a header!!
+            <img src={Logo} />
         </header>
     )
 };
