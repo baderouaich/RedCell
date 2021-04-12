e681b620c68a120f1ba6e3d2bde15a55474efce7
BaderEddineOuaich <badereddineouaich@gmail.com>
2021-04-12 16:45:48 +0000

    Added custom fonts
    deleted unecessary files and code
    
    installed node-sass .scss dependency

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = props => 
+{
+    return (
+        <header className="header">
+            I am a header!!
+        </header>
+    )
+};
+
+export default Header;
